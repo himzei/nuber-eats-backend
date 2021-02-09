429fe75d8b5cce497473024738ce0ca11a11e12b
himzei <himzei@himzeiui-MacBookPro.local>
2021-02-09 20:59:55 +0900

    4.7

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,28 +1,45 @@
-import { Field, InputType, ObjectType, registerEnumType } from '@nestjs/graphql';
+import {
+  Field,
+  InputType,
+  ObjectType,
+  registerEnumType,
+} from '@nestjs/graphql';
 import { CoreEntity } from 'src/common/entities/core.entity';
-import { Column, Entity } from 'typeorm';
+import { BeforeInsert, Column, Entity } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { InternalServerErrorException } from '@nestjs/common';
 
 enum UserRole {
-  Client, 
-  Owner, 
+  Client,
+  Owner,
   Delivery,
 }
 
-registerEnumType(UserRole, {name: "UserRole"})
+registerEnumType(UserRole, { name: 'UserRole' });
 
-@InputType({ isAbstract: true})
+@InputType({ isAbstract: true })
 @ObjectType()
 @Entity()
 export class User extends CoreEntity {
   @Column()
-  @Field(type => String)
+  @Field((type) => String)
   email: string;
 
   @Column()
-  @Field(type => String)
+  @Field((type) => String)
   password: string;
 
-  @Column({type: "enum", enum: UserRole})
-  @Field(type => UserRole)
+  @Column({ type: 'enum', enum: UserRole })
+  @Field((type) => UserRole)
   role: UserRole;
+
+  @BeforeInsert()
+  async hashPassword(): Promise<void> {
+    try{
+      this.password = await bcrypt.hash(this.password, 10);
+    }catch(e){
+      console.log(e) 
+      throw new InternalServerErrorException();
+    }
+  }
 }
