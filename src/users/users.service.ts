b9528c320b6404abc83b70b929471c496f502927
himzei <himzei@himzeiui-MacBookPro.local>
2021-02-10 16:24:24 +0900

    5.0

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { boolean } from 'joi';
 import { Repository } from 'typeorm';
 import { CreateAccountInput } from './dtos/create-account.dto';
+import { LoginInput } from './dtos/login.dto';
 import { User } from './entities/user.entity';
 
 @Injectable()
@@ -14,18 +16,48 @@ export class UsersService {
     email,
     password,
     role,
-  }: CreateAccountInput): Promise<{ok: boolean; error?: string}> {
+  }: CreateAccountInput): Promise<{ ok: boolean; error?: string }> {
     try {
       const exists = await this.users.findOne({ email });
       if (exists) {
-        return {ok: false, error: 'There is a user with that email already'};
+        return { ok: false, error: 'There is a user with that email already' };
       }
       await this.users.save(this.users.create({ email, password, role }));
-      return {ok: true}
+      return { ok: true };
     } catch (e) {
       // make error
-      return {ok: false, error: "]Couldn't create account"};
+      return { ok: false, error: "]Couldn't create account" };
     }
     // create user & hash the password
+  }
+  async login({
+    email,
+    password,
+  }: LoginInput): Promise<{ ok: boolean; error?: string; token?: string }> {
+    try {
+      const user = await this.users.findOne({ email });
+      if (!user) {
+        return {
+          ok: false,
+          error: 'user not found',
+        };
+      }
+      const passwordCorrect = await user.checkPassword(password);
+      if (!passwordCorrect) {
+        return {
+          ok: false,
+          error: 'wrong password',
+        };
+      }
+      return {
+        ok: true, 
+        token: "lalala"
+      }
+    } catch (error) {
+      return {
+        ok: false,
+        error,
+      };
+    }
   }
 }
